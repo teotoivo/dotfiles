4c72cbf27507e7c75a5f47efe3b4b4522b4199dd
teotoivo <teo.maximilien@gmail.com>
2025-03-09 16:10:17 +0200

    fish config changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,15 +1,43 @@
 # FZF configuration with bat for preview
 set -gx FZF_DEFAULT_OPTS "--preview 'bat --color=always --style=numbers --line-range=:500 {}'"
 
-# If you want to use bat for CTRL+T file search
-set -gx FZF_CTRL_T_OPTS "--preview 'bat --color=always --style=numbers --line-range=:500 {}'"
+function fzfp
+  set base_dir $argv[1]
 
-# For directory preview with CTRL+T you might want to use ls or exa/eza
-set -gx FZF_ALT_C_OPTS "--preview 'ls -la {}'"
+  if test -z "$base_dir"
+    set base_dir "."
+  end
 
-# For the Fish plugin
-set -g fzf_preview_file_cmd "bat --style=numbers --color=always"
-set -g fzf_preview_dir_cmd "ls -la"
+  set file (find "$base_dir" -type f -name '.*' -or -type f 2>/dev/null | fzf --preview "bat --color=always {}")
+
+  if test -n "$file"
+    # Use $EDITOR or default to nvim if EDITOR is not set
+    if set? EDITOR
+      $EDITOR "$file"
+    else
+      nvim "$file"
+    end
+  end
+end
+
+function fcd
+  set base_dir $argv[1]
+  if test -z "$base_dir"
+    set base_dir "."
+  end
+
+  set dir (find "$base_dir" -type d -name '.*' -or -type d 2>/dev/null | fzf --preview "ls --color=auto -lh {}")
+
+  if test -n "$dir"
+    cd "$dir"
+  else
+    echo "No directory selected"
+  end
+end
+
+# Bind Ctrl+F to fzfp.  Note that fish uses different bind syntax
+bind \cf fzfp
+
 
 function fish_greeting
 
