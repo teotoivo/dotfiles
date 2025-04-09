99353aff235de7bc847c9738f3c1dcd4780757be
teotoivo <teo.maximilien@gmail.com>
2025-04-09 16:13:38 +0300

    something

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -1,14 +1,23 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-local function map(mode, lhs, rhs, desc)
-  vim.keymap.set(mode, lhs, rhs, { desc = desc, noremap = true, silent = true })
-end
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
 local dap = require("dap")
 
-map("n", "<F5>", dap.continue, "DAP: Continue")
-map("n", "<F10>", dap.step_over, "DAP: Step Over")
-map("n", "<F11>", dap.step_into, "DAP: Step Into")
-map("n", "<F12>", dap.step_out, "DAP: Step Out")
+-- DAP mappings with proper options and descriptions
+map("n", "<F5>", dap.continue, { desc = "DAP: Continue", noremap = true, silent = true })
+map("n", "<F9>", dap.step_over, { desc = "DAP: Step Over", noremap = true, silent = true })
+map("n", "<F10>", dap.step_into, { desc = "DAP: Step Into", noremap = true, silent = true })
+map("n", "<F11>", dap.step_out, { desc = "DAP: Step Out", noremap = true, silent = true })
+
+-- Toggle Terminal (works in all modes)
+map({ "n", "i", "v", "t" }, "<A-h>", function()
+  Snacks.terminal() -- make sure this function exists in your runtime
+end, { desc = "Toggle Terminal", noremap = true, silent = true })
+
+-- Buffer navigation
+map("n", "<Tab>", ":bnext<CR>", opts)
+map("n", "<S-Tab>", ":bprevious<CR>", opts)
+
+-- Scroll up/down and center the cursor
+map("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center", noremap = true, silent = true })
+map("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center", noremap = true, silent = true })
